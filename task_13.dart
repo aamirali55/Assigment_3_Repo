4c4818668bf8e7d54d7e3201fdc053ada1651cd6
Aamir ali <aamiralisoomro73@gmail.com>
2024-07-23 21:24:41 -0700

    changes

--- task_13.dart
+++ task_13.dart
@@ -5,10 +5,9 @@ void main() {
   int numberOfRows = int.parse(stdin.readLineSync()!);
 
   for (int i = 1; i <= numberOfRows; i++) {
-    String row = '';
     for (int j = 1; j <= i; j++) {
-      row += '$i';
+      stdout.write('$i');
     }
-    print(row);
+    print(" ");
   }
 }
--- task_14.dart
+++ task_14.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
 
 void main() {
-   print("Enter the Number of Rows");
+  print("Enter the Number of Rows");
   int numberOfRows = int.parse(stdin.readLineSync()!);
   int number = 1;
 
   for (int i = 1; i <= numberOfRows; i++) {
-    String row = '';
     for (int j = 1; j <= i; j++) {
-      row += '$number '; 
+      stdout.write('$number ');
       number++;
     }
-    print(row); 
+    print(" ");
   }
 }
